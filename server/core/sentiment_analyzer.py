f18f32f32eb5440945fca2a41c461b52611abae5
José Silva Monard <silvamonard@protonmail.com>
2020-07-17 13:34:18 -0500

    uses single ensemble interface for custom sentiment estimator

--- server/core/sentiment_analyzer.py
+++ server/core/sentiment_analyzer.py
@@ -1,21 +1,17 @@
 from textblob import TextBlob
-from classifier import SentimentClassifier
 from collections import Counter
-from sentiment_classifier import BasicSentimentClassifier
+from sentiment_estimator import SentimentEstimator
 
 class SentimentAnalyzer:
     def __init__(self):
-        self.__sent_py = SentimentClassifier()
-        self.__nn_estimator = BasicSentimentClassifier.newinstance(context_path='../sentiment-estimator/data/')
-        self.__nb_estimator = BasicSentimentClassifier.newinstance('nb', context_path='../sentiment-estimator/data/')
+        self.__sentiment_estimator = SentimentEstimator(context_path='../sentiment-estimator/data/')
     
     def analyze(self, text, usetb=False):
-        sentiment = (self.__nn_estimator.predict(text) + self.__nb_estimator.predict(text)) / 4.0
-        sentiment += self.__sent_py.predict(text)
-        estimators = 2.0
+        sentiment = self.__sentiment_estimator.predict(text) / 2.0
+        estimators = 1.0
         if usetb:
             sentiment += self.__textblob_analysis(text)
-            estimators += 1.0
+            estimators = 2.0
         return self.__categorize(sentiment / estimators)
     
     def __textblob_analysis(self, text):
